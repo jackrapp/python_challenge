8625f33816e588efacad9015e2c88adb9188abef
jackrapp <jacklyn.rapp@gmail.com>
2018-12-05 17:20:53 -0600

    final final pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -26,19 +26,31 @@ with open(budget_data, newline="") as csvfile:
         profit = profit + value
         #greatest increase in profits
         increase = max(increase, value)
+        if increase == value:
+            inc_month = row[0]
         #greatest decrease in losses
         decrease = min(decrease, value)
+        if decrease == value:
+            dec_month = row[0]
 
     #total net profit/loss
     avg_monthly = int(profit)/int(months)
 
-#print
-print('''
-Financial Analysis
-------------------''')
+print("Financial Analysis")
+print("------------------")
 print(f"Total Months: {months}")
 print(f"Total: {profit}")
 print(f"Average Change: {avg_monthly}")
-print(f"Greatest Profit: {increase}")
-print(f"Greatest Loss: {decrease}")
+print(f"Greatest Profit: {inc_month} ({increase})")
+print(f"Greatest Loss: {dec_month} ({decrease})") 
+
+#print out results and send as text file
+with open("pybank_analysis.txt", "w") as pybank_analysis:
+    print("Financial Analysis", file = pybank_analysis)
+    print("------------------", file = pybank_analysis)
+    print(f"Total Months: {months}", file = pybank_analysis)
+    print(f"Total: {profit}", file = pybank_analysis)
+    print(f"Average Change: {avg_monthly}", file = pybank_analysis)
+    print(f"Greatest Profit: {inc_month} ({increase})", file = pybank_analysis)
+    print(f"Greatest Loss: {dec_month} ({decrease})",file = pybank_analysis)    
 
